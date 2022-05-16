e1ec180c26c6edfe26fbee966a0a44f473f705f4
user.email <dtramgre@srv604.webhostingforstudents.com>
2022-05-15 23:44:00 -0400

    bare bone display information for user

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
+//Start session()
+session_start();
 //Require the autoload file
 require_once('vendor/autoload.php');
 
@@ -18,6 +20,62 @@ $f3->route('GET /', function () {
     echo $view->render('views/home.html');
 }
 );
+
+// Create a route for personal profile
+$f3->route('GET /personal', function () {
+
+
+    $view = new Template();
+    echo $view->render('views/personal.html');
+}
+);
+
+// Create a route for personal profile
+$f3->route('GET|POST /profile', function () {
+
+    //This is Tina's diner code that needs to be modified
+
+
+
+
+
+// end of Tina's code
+
+
+    $_SESSION['fname'] = $_POST['fname'];
+    $_SESSION['lname'] = $_POST['lname'];
+    $_SESSION['gender'] = $_POST['gender'];
+    $_SESSION['age'] = $_POST['age'];
+    $_SESSION['phone'] = $_POST['phone'];
+    $_SESSION['food'] = $_POST['food'];
+
+
+    $view = new Template();
+    echo $view->render('views/profile.html');
+});
+
+// Create a route for interests profile
+$f3->route('GET|POST /interests', function () {
+
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['state'] = $_POST['state'];
+    $_SESSION['seeking'] = $_POST['seeking'];
+    $_SESSION['biography'] = $_POST['biography'];
+    $view = new Template();
+    echo $view->render('views/interests.html');
+});
+
+// Create a route for profile summary
+$f3->route('GET|POST /summary', function () {
+
+$_SESSION['interests'] = implode(", ", $_POST['interests']);
+
+    $view = new Template();
+    echo $view->render('views/summary.html');
+}
+);
+
+
 
 //Run fat free
 $f3->run();
